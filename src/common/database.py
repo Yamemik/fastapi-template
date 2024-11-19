e2a07c3c741d0f60e83db7f30b72dcbe0ace20e6
kkk <kuancarlos@mail.ru>
2024-11-19 17:42:58 +0500

    upd common

--- src/common/database.py
+++ src/common/database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = os.getenv('STRING_CONNECT')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
